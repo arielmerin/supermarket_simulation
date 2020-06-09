81c8328b8f79160c28d78e072200a6e51d23b7da
arielmerin <arielmerino@ciencias.unam.mx>
2020-06-08 19:09:39 -0500

    Corrigiendo el main
    sdga error del pull

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,6 @@ import util.Lista;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Lista<String> plotear = new Lista<>();
         for (int i = 1; i < 15; i++) {
 
@@ -33,52 +32,6 @@ public class Main {
             }
             simulation.generarReportes();
             plotear.agregar(simulation.escribeLinea());
-=======
-        Menu menu = new Menu();
-        //menu.principal();
-
-            SuperMarket superMarket = new SuperMarket(11,12,15);
-            /*
-            superMarket.cargarProductos("productos.txt");
-            superMarket.getAlmacen().agregarProducto(new Product(3,"pug", (float) 2.2));
-            superMarket.getAlmacen().agregarProducto(new Product(2,"perro Salchicha", (float) 2.2));
-            superMarket.getAlmacen().agregarProducto(new Product(1,"alimento para perro", (float) 2.2));
-            for (int j = 0; j < 97; j++) {
-                superMarket.formandoCliente();
-            }
-
-             */
-
-            System.out.println("Estos son los productos con pocas existas");
-            Serializer serializer = new Serializer();
-            Date fecha = new Date();
-            SimpleDateFormat sdt = new SimpleDateFormat(("dd_MM_yyyy(HH:mm:ss)"));
-            String nombre = sdt.format(fecha);
-            serializer.creaCarpeta("Tickets");
-            serializer.creaCarpeta("ReportesDiarios");
-            serializer.creaCarpeta("Faltantes");
-            serializer.escribeTXT(superMarket, "ReportesDiarios"+ "/"+ "[reporte]"+nombre+".txt");
-            serializer.escribeTXT(superMarket.reportePocasExistencias(), "Faltantes"+ "/"+ "[faltantes]"+nombre+".txt");
-            serializer.escribeTXT(superMarket.getTickets(), "Tickets"+ "/"+ "[tickets]" +nombre+".txt");
-            System.out.println("lleva ");
-
-        ProductoBuilder productoBuilder = new ProductoBuilder();
-        Lista<String> textos = new Lista<>();
-        for (int i = 0; i < 10000; i++) {
-            textos.agregar(String.valueOf(productoBuilder.next()));
-        }
-        serializer.escribeTXT(textos,"prodctosAlea.txt");
-        /*
-        Simulation simulation = new Simulation(7,8,150,500);
-        simulation.simular();
-        simulation.generarReportes();
-        */
-        Simulation simulation = new Simulation(7,8,150,2);
-        try {
-            simulation.simular();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
->>>>>>> 2922d536a40aeaa2c130e9af49c0edbbebbdc877
         }
         Serializer serializer = new Serializer();
         serializer.escribeTXT(plotear, "datos.p");
